47a57eaecb11ebdf29b5564576f8eb6c1ce4c317
Dewkong <kryniu113@gmail.com>
2022-01-07 15:09:44 +0100

    changed time to water; removed sensor and humidity; publish message on "rain" topic

--- sprinkler.py
+++ sprinkler.py
@@ -6,9 +6,8 @@ import mosquitto
 
 mqttc_server = sys.argv[1]
 id = int(sys.argv[2])
+time_to_water = int(sys.argv[3])
 sector_id = None
-sensor_id = None
-current_humidity = -1
 
 mqttc = mosquitto.Mosquitto()
 
@@ -16,7 +15,7 @@ def on_connect(mqttc, obj, rc):
     print("rc: "+ str(rc))
 
 def on_message(mqttc, obj, msg):
-    global sector_id, sensor_id, current_humidity
+    global sector_id
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
     if msg.topic == "agh/iot/project9/config":
         try:
@@ -25,18 +24,23 @@ def on_message(mqttc, obj, msg):
                 for sprinkler in sector["sprinklers"]:
                     if sprinkler == id:
                         sector_id = int(sector["id"])
-                        sensor_id = int(sector["sensor_id"])
-                        mqttc.subscribe("agh/iot/project9/sensor/" + str(sensor_id) + "/humidity", 0)
         except Exception as e:
             print("json with incorrect format, " + str(e))
     elif msg.topic == "agh/iot/project9/active_sector":
         if int(msg.payload) == sector_id:
             mqttc.publish("agh/iot/project9/sprinkler/" + str(id) + "/state", "1", 2, False)
-            time_to_sleep = (11 - (current_humidity/10)) if current_humidity != -1 else 5
-            time.sleep(time_to_sleep)
+            before_watering = {
+                "is_watering": True,
+                "sprinkler_id": id
+            }
+            mqttc.publish("agh/iot/project9/simulation/area" + str(sector_id) + "/rain", json.dumps(before_watering), 2, False)
+            time.sleep(time_to_water)
+            after_watering = {
+                "is_watering": False,
+                "sprinkler_id": id
+            }
+            mqttc.publish("agh/iot/project9/simulation/area" + str(sector_id) + "/rain", json.dumps(after_watering), 2, False)
             mqttc.publish("agh/iot/project9/sprinkler/" + str(id) + "/state", "0", 2, False)
-    elif msg.topic == "agh/iot/project9/sensor/" + str(sensor_id) + "/humidity":
-        current_humidity = int(msg.payload)
 
 
 def on_publish(mqttc, obj, mid):
